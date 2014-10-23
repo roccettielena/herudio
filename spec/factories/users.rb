0949586ebc41abdde9cbf76320940933d6063f2a
Alessandro Desantis <desa.alessandro@gmail.com>
2014-10-23 17:00:04 +0200

    Fix user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    name { Faker::Name.name }
+    full_name { Faker::Name.name }
     email { Faker::Internet.email }
     password { SecureRandom.hex }
     confirmed_at { Time.now }
