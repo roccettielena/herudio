c0db2a581d8223e19a8c352da067c616d7e6fc7b
Alessandro Desantis <desa.alessandro@gmail.com>
2014-10-29 18:48:38 +0100

    Add ActionMailer SMTP configuration

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,16 @@ module Herudio
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
+
+    # ActionMailer SMTP configuration.
+    config.action_mailer.smtp_settings = {
+      address:              ENV['SMTP_ADDRESS'],
+      port:                 ENV['SMTP_PORT'],
+      domain:               ENV['SMTP_DOMAIN'],
+      user_name:            ENV['SMTP_USER'],
+      password:             ENV['SMTP_PASS'],
+      authentication:       ENV['SMTP_AUTH'],
+      enable_starttls_auto: (ENV['SMTP_STARTTLS'] == 'true')
+    }
   end
 end
