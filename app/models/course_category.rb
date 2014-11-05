018f9ddbed91f215252da912221563ec6bb89c68
Alessandro Desantis <desa.alessandro@gmail.com>
2014-11-05 20:31:08 +0100

    Fix an issue in the CourseCategory -> Course association

--- app/models/course_category.rb
+++ app/models/course_category.rb
@@ -1,5 +1,5 @@
 class CourseCategory < ActiveRecord::Base
-  has_many :courses, dependent: :restrict_with_error, inverse_of: :category
+  has_many :courses, dependent: :restrict_with_error, inverse_of: :category, foreign_key: 'category_id'
 
   validates :name, presence: true, uniqueness: true
 end
