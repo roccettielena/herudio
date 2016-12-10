78cbe2e75baf4fdab66b0edb4f9141d62a67a1d0
Alessandro Desantis <desa.alessandro@gmail.com>
2016-12-10 19:26:06 +0100

    Remove datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require pickadate/lib/picker
-//= require pickadate/lib/picker.date
-//= require pickadate/lib/translations/it_IT
 //= require cocoon
 //= require_tree .
